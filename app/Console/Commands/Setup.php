175600e74ee361e3addd03f45cdbb613e1ae7185
JamesBillate <james.billate@ciit.edu.ph>
2025-08-04 08:46:23 +0800

    x

---
+++ app/Console/Commands/Setup.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Setup extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:setup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->runProcess(['composer', 'install'], "[STEUP]: updating composer");
+        $this->runProcess(['npm', 'install'], "[STEUP]: node and vite composer");
+
+        if (!file_exists(base_path('.env'))) {
+            copy(base_path('.env.example'), base_path('.env'));
+            $this->info('[STEUP]: .env generated');
+        } else {
+            $this->warn('[STEUP]: .env failed to generate');
+        }
+
+        $this->call('key:generate');
+        $this->info('[STEUP]: generated a key');
+        $this->call('migrate', ['--force' => true]);
+        $this->info('[STEUP]: migrated database');
+        $this->call('db:seed', ['--force' => true]);
+        $this->info('[STEUP]: seeded the database');
+    }
+
+    private function runProcess(array $command, string $message)
+    {
+        $this->info("{$message}");
+        $process = new \Symfony\Component\Process\Process($command, base_path());
+        $process->setTimeout(null);
+        $process->run(function ($type, $buffer) {
+            echo $buffer;
+        });
+
+        if (!$process->isSuccessful()) {
+            $this->error("{$message} failed!");
+            exit(1);
+        }
+    }
+}
