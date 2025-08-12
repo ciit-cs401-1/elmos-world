c5e25957edd716c3a7a638a620e380e381eb67fc
Michael <90962435+Michael679089@users.noreply.github.com>
2025-08-12 17:04:07 +0800

    fixed the show

--- resources/views/components/dashboard/posts/show.blade.php
+++ resources/views/components/dashboard/posts/show.blade.php
@@ -41,7 +41,7 @@
             <svg class="h-3 w-3 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"/>
             </svg>
-            {{ $post->publication_date ? $post->publication_date->format('M d, Y') : 'Not yet published' }}
+            {{ date('M d, Y', strtotime($post->publication_date)) }}
         </span>
         <span class="flex gap-1 items-center">
             <svg class="h-5 w-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
